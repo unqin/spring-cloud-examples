275f3136322639348dee5b1242a05f9b5111838a
StevenGuo <isguoyq@gmail.com>
2020-01-06 16:24:44 +0800

    update eureka

---
+++ eureka-producer-consumer/spring-cloud-consumer/src/main/java/com/example/springcloudconsumer/SpringCloudConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.example.springcloudconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableFeignClients
+public class SpringCloudConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsumerApplication.class, args);
+    }
+
+}
--- eureka-producer-consumer/spring-cloud-eureka/src/main/java/com/example/springcloudeureka/SpringCloudEurekaApplication.java
+++ eureka-producer-consumer/spring-cloud-eureka/src/main/java/com/example/springcloudeureka/SpringCloudEurekaApplication.java
@@ -2,8 +2,10 @@ package com.example.springcloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //申明这是一个Eureka服务
 public class SpringCloudEurekaApplication {
 
     public static void main(String[] args) {
