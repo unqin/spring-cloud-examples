a86b8b7c6dfe8446e21dba21881d5ca63171e144
unchin <isguoyq@gmail.com>
2020-03-24 14:42:13 +0800

    [UPD] update oauth2 && JWT 1.0

--- oauth2-server/src/main/java/com/example/oauth2server/config/AuthorizationServerConfig.java
+++ oauth2-server/src/main/java/com/example/oauth2server/config/AuthorizationServerConfig.java
@@ -1,7 +1,9 @@
 package com.example.oauth2server.config;
 
+import com.example.oauth2server.component.JwtTokenEnhancer;
 import com.example.oauth2server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +11,14 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 认证服务器配置
@@ -26,13 +36,35 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private UserService userService;
 
+    @Autowired
+//    @Qualifier("redisTokenStore")
+    @Qualifier("jwtTokenStore")
+    private TokenStore tokenStore;
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+    @Autowired
+    private JwtTokenEnhancer jwtTokenEnhancer;
+
     /**
      * 使用密码模式需要配置
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        //登录时配置
+//        endpoints.authenticationManager(authenticationManager)
+//                .userDetailsService(userService);
+
+        //jwt单点登录时配置
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        List<TokenEnhancer> delegates = new ArrayList<>();
+        delegates.add(jwtTokenEnhancer); //配置JWT的内容增强器
+        delegates.add(jwtAccessTokenConverter);
+        enhancerChain.setTokenEnhancers(delegates);
         endpoints.authenticationManager(authenticationManager)
-                .userDetailsService(userService);
+                .userDetailsService(userService)
+                .tokenStore(tokenStore) //配置令牌存储策略
+                .accessTokenConverter(jwtAccessTokenConverter)
+                .tokenEnhancer(enhancerChain);
     }
 
     @Override
@@ -42,8 +74,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret(passwordEncoder.encode("admin123456"))//配置client-secret
                 .accessTokenValiditySeconds(3600)//配置访问token的有效期
                 .refreshTokenValiditySeconds(864000)//配置刷新token的有效期
-                .redirectUris("http://www.baidu.com")//配置redirect_uri，用于授权成功后跳转
+//                .redirectUris("http://www.baidu.com")//配置redirect_uri，用于授权成功后跳转
+
+                .redirectUris("http://localhost:9501/login") //单点登录时配置
+                .autoApprove(true) //自动授权配置
+
                 .scopes("all")//配置申请的权限范围
-                .authorizedGrantTypes("authorization_code","password");//配置grant_type，表示授权类型
+                .authorizedGrantTypes("authorization_code","password","refresh_token");//配置grant_type，表示授权类型
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.tokenKeyAccess("isAuthenticated()"); // 获取密钥需要身份认证，使用单点登录时必须配置
     }
 }
--- oauth2-server/src/main/java/com/example/oauth2server/controller/UserController.java
+++ oauth2-server/src/main/java/com/example/oauth2server/controller/UserController.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by macro on 2019/9/30.
- */
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
